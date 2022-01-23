06e0eac90a26a077bc7b74a5673d98ade78d8d1b
mf-3d <mf72cli120@gmail.com>
2022-01-23 15:18:39 +0900

    2022-01-23 Update
    ファイルを追加

---
+++ main.js
@@ -0,0 +1,38 @@
+const electron = require("electron");
+
+var nodeStatic = require('node-static');
+var file = new nodeStatic.Server(__dirname + '/src');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(1210);//ポートは空いていそうなところで。
+
+function nw(){
+  win=new electron.BrowserWindow({
+    width: 400,
+    height: 500,
+    transparent: true,
+    frame: false,
+    toolbar: false,
+    alwaysOnTop: true,
+    icon: `${__dirname}/src/icon.png`,
+    webPreferences: {
+      preload: `${__dirname}/src/preload.js`
+    }
+  })
+  win.webContents.loadURL(`http://localhost:1210`);
+
+  win.webContents.on('close',()=>{
+      win=null;
+  })
+}
+
+electron.app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+electron.app.on('ready',nw);
---
+++ src/Bluesky/readme.txt
@@ -0,0 +1,2 @@
+このMMDモデルを使用する場合クレジットにmf7cliを記載してください。
+Texture by textures.com
